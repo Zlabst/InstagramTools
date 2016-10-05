71387f182138f86d63d56945da199764eb7b8da9
TheBottle <TheBottle@DESKTOP-GI7991P>
2016-10-05 16:38:56 +0600

    Like update

---
+++ InstagramTools/Instagram.API/LikePhoto.cs
@@ -0,0 +1,35 @@
+﻿using Extreme.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramTools
+{
+    class LikePhoto
+    {
+        public static string Like(CookieDictionary Cookie, string Url)
+        {
+            //Потом доделаю
+            using (var request = new HttpRequest())
+            {
+                string CSRF = null;
+                Cookie.TryGetValue("csrftoken", out CSRF);
+
+                request.Cookies = Cookie;
+                request.Referer = Url;
+
+                request.UserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile Safari/600.1.4";
+
+                request.AddHeader("X-CSRFToken", CSRF)
+                       .AddHeader("X-Instagram-AJAX", "1")
+                       .AddHeader("X-Requested-With", "XMLHttpRequest");
+
+                var response = request.Post(Url);
+
+                return response.ToString();
+            }
+        }
+    }
+}
--- InstagramTools/Program.cs
+++ InstagramTools/Program.cs
@@ -11,9 +11,12 @@ namespace InstagramTools
     {
         static void Main(string[] args)
         {
-            if (Authorization.Login("TheBottleCode", "F1NPidor") != null)
+            var Auth = Authorization.Login("TheBottleCode", "F1NPidor");
+            if (Auth != null)
             {
                 Console.WriteLine("Успешно авторизировались");
+                //Потом доделаю
+                Console.WriteLine(LikePhoto.Like(Auth.Cookies, "https://www.instagram.com/p/BLG0X6Cj8Ha/"));
             }
             else Console.WriteLine("Логин или пароль введен не правильно");
                 
